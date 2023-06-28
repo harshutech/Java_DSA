81d069a4c8219a8c148504ec34b44f0cfb9d0015
Harsh patil <107917540+harshutech@users.noreply.github.com>
2023-06-28 18:27:23 +0530

    While loop question completed❤️

---
+++ loops/print1to10.java
@@ -0,0 +1,16 @@
+class print1to10{
+    public static void main(String args[]){
+        int counter = 1;
+
+        while(counter<=10){
+            // normal code 
+            System.out.println(counter);
+            counter ++;
+
+            // minimise code 
+            // System.out.println(counter++);
+
+            
+        }
+    }
+}
